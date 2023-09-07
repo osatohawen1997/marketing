73899746a7c6952f21268e42a77453cbbd742c96
osatohawen1997 <idahosavictor925@gmail.com>
2023-09-07 14:28:42 -0700

    responsive template

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+// js code for index.html
+
+anime ({
+    targets: '.review',
+    innerHTML: [0, 5],
+    duration: 3000,
+    round: 1,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '.years',
+    innerHTML: [0, 10],
+    duration: 3000,
+    round: 1,
+    easing: 'easeInOutExpo'
+});
+
+anime ({
+    targets: '.user',
+    innerHTML: [0, 25],
+    duration: 3000,
+    round: 1,
+    easing: 'easeInOutExpo'
+});
+
+anime ({
+    targets: '.smile',
+    innerHTML: [0, 21.5],
+    duration: 3000,
+    round: 1,
+    easing: 'easeInOutExpo'
+});
